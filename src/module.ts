77d470f213360437413f994d7715b812e21e5403
Gábor Torma <gabor@torma.dev>
2025-04-23 16:50:02 +0200

    fix: module name

--- src/module.ts
+++ src/module.ts
@@ -6,7 +6,7 @@ export interface ModuleOptions {
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'nuxt-feathers-pinia',
+    name: '@gabortorma/nuxt-feathers-pinia',
     configKey: 'feathersPinia',
     compatibility: {
       nuxt: '^3.0.0',
